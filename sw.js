c7e50fa2b1729054c12654bafbecf9e8ca430324
coadalaurentiu5-png <coadalaurentiu5@gmail.com>
2026-08-09 21:38:26 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+{
+  "name": "NovaX Comenzi",
+  "short_name": "NovaX",
+  "start_url": "/orders-app.html",
+  "display": "standalone",
+  "background_color": "#06090e",
+  "theme_color": "#0d131f",
+  "orientation": "portrait"
+}
